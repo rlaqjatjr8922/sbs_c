126444ca8aff2ffc6fb78a8c7d87d0a1f7c197ce
rlaqjatjr8922 <gimbeomseognam5@gmail.com>
2022-01-18 14:34:14 +0900

    4

---
+++ c언어/c언어/Day04/ex01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+	int main(void){	
+        int num = 3;
+        printf("정수");
+    scanf("%d", &num);  
+
+
+        if(num< 3)
+        printf(" 작다\n");
+
+        if(num== 3)
+        printf(" 3=n\n");
+
+        if(num> 3)
+        printf(" 크다\n");
+
+
+	return 0;}
