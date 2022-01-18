e39e0c90f05b84a48112f4ab94449508b6558b3a
rlaqjatjr8922 <gimbeomseognam5@gmail.com>
2022-01-18 14:27:33 +0900

    4일차

---
+++ 새 폴더/c언어/Day03/ex10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+	int main(void){	
+
+    int num1 = 20, num2 = 10;
+    int resul;
+
+    resul = 1 && 1;
+    printf("result 1 : %d\n" ,resul);
+
+
+    result = num1 == 20 && num2 == 10 ;
+    printf("result 2 : %d\n" ,result);
+
+
+    esult = num1 == 20 && num2 == 100 ;
+    printf("resut 3 : %d \n",result);
+
+	return 0;}
