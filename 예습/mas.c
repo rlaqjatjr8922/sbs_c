e345d4d7221a58782a0fcbc10463907314400c6a
rlaqjatjr8922 <gimbeomseognam5@gmail.com>
2022-01-25 14:36:00 +0900

    as

--- 예습/mas.c
+++ 예습/mas.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+void main() {
 printf ("시작");
 int n;
 int x;
 int y;
 for (int i = 0;i <= 100; i++ ){
+    printf("%d",i);
     x = rand() % 100 +1;
-    y = rend() % 10 + 1;
+    y = rand() % 10 + 1;
     printf("%d x %d = ",x,y);
     scanf("%d",&n);
     n = (int)n;
@@ -17,7 +18,7 @@ for (int i = 0;i <= 100; i++ ){
                     }
     else{
         printf("오답~~~~~~ 한문재더");
-        i = i + 2;
+        i = i - 2;
     }
     
 printf("%d",i);
