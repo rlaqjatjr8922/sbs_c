d57c3713536ce79ba07a2268b09d4ea510f80e1c
rlaqjatjr8922 <gimbeomseognam5@gmail.com>
2022-02-03 13:31:57 +0900

    ㅐㅐ]

--- c언어/예습/mas.c
+++ c언어/예습/mas.c
@@ -6,12 +6,12 @@ int n;
 int x;
 int y;
 char q[10] = {'1','1','1','1','1','1','1','1','1','1',};
-
+/*
 for (int qwe = 0; qwe < 9; qwe++)
 {
-    q[qwe] = 1; 
+    q[qwe] = '1'; 
 }
-printf("%s",q[1]);
+printf("%s",q[1]);*/
 
 
 for (int i = 1;i <= 2; i++ ){
@@ -22,37 +22,37 @@ for (int i = 1;i <= 2; i++ ){
     n = (int)n;
     if((x * y) == n){
         printf("정답\n");
-        q[i] = ("정답  %d x %d = %d",x,y,n);
+//        q[i] = ("정답  %d x %d = %d",x,y,n);
                     }
     else{
         printf("오답~~~~~~ 한문재더\n");
         i = i - 2;
-       q[i] = ("      %d x %d = %d 정답=%d",x,y,n,(x*y));
+//       q[i] = ("      %d x %d = %d 정답=%d",x,y,n,(x*y));
     }
     
 
 }
-<<<<<<< HEAD
-printf("%s",q[1]);
-=======
 
->>>>>>> eb6e0156c12cdcca05342fb2a3a55b001025b189
+//printf("%s",q[1]);
+
+
+
 printf ("축하함니다 (암호:124)");
 
 
 
-<<<<<<< HEAD
+
 for (int qw = 0; qw < 5;qw++)
 {//수정
     printf("%s",q[qw]);
 }
 
 printf ("끝");
-=======
+
 
 printf ("끝");
 
->>>>>>> eb6e0156c12cdcca05342fb2a3a55b001025b189
+
 
 return 0;
 
---
+++ c언어/예습/ㄴㅁㅇㄴㅁㅇㄴ.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    char q[10] ="ddddddddq";
+
+//for (int i = 0; i >= 10; i++)
+//{
+ //   printf("%s",q[i]);
+//}
+
+
+
+
+
+    printf("%s",q);
+return 0;
+
+}
